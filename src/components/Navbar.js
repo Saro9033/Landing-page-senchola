ee64fe1c1b856ae98df08a05c7307e66e22e661d
Saravanan S <88275659+Saro9033@users.noreply.github.com>
2024-04-23 11:44:26 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,8 @@ const Navbar = ({openModal, openOutModal}) => {
         <nav id='navbar' className="navbar navbar-expand-sm px-1 navbar-light"  style={{backgroundColor:'white'}  } >
             <div className="container-fluid"> 
             <a href="/" className="navbar-brand text-info" >
-		  <img width='100px' src={Logo} alt="Logo" /> 
+		 /*  <img width='100px' src={Logo} alt="Logo" /> */
+	   <h3>Speedy</h3>
 	    </a>   
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span className="navbar-toggler-icon"></span>
